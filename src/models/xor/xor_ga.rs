e4604e48f5b6fd82e9013de7dd1100f58261f61b
Caden Miller <cwmiller4030@gmail.com>
2022-03-01 20:51:25 -0600

    simplified mutation function

--- src/models/xor/xor_ga.rs
+++ src/models/xor/xor_ga.rs
@@ -1,5 +1,4 @@
 use rand::prelude::*;
-use rand_distr::StandardNormal;
 use crate::matrix;
 use crate::models::xor;
 
@@ -46,122 +45,14 @@ fn score_population(population: &Vec<Individual>) -> (f32, f32, Vec<Individual>,
     (best_fitness, average_fitness, updated_pop, best_index)
 }
 
-fn mutate(a: &xor::XorModel, mutation_rate: f32, std: f32) -> xor::XorModel {
+fn mutate(a: &xor::XorModel, std: f32) -> xor::XorModel {
     let mut b = a.clone();
 
-    // fc1
-    // weights
-    for i in 0..a.fc1.weights.value.len() {
-        let m = thread_rng().gen::<f32>() % 1.0;
+    let mut mutation = xor::new_gaussian_noise();
+    mutation = xor::scalar(&mutation, std);
 
-        if m <= mutation_rate {
-            let w: f32 = thread_rng().sample(StandardNormal);
-            b.fc1.weights.value[i] += w * std;
-        }
-    }
-
-    // bias
-    for i in 0..a.fc1.bias.value.len() {
-        let m = thread_rng().gen::<f32>() % 1.0;
-
-        if m <= mutation_rate {
-            let w: f32 = thread_rng().sample(StandardNormal);
-            b.fc1.bias.value[i] += w * std;
-        }
-    }
-
-    // fc2
-    //weights
-    for i in 0..a.fc2.weights.value.len() {
-        let m = thread_rng().gen::<f32>() % 1.0;
-
-        if m <= mutation_rate {
-            let w: f32 = thread_rng().sample(StandardNormal);
-            b.fc2.weights.value[i] += w * std;
-        }
-    }
-
-    // bias
-    for i in 0..a.fc2.bias.value.len() {
-        let m = thread_rng().gen::<f32>() % 1.0;
-
-        if m <= mutation_rate {
-            let w: f32 = thread_rng().sample(StandardNormal);
-            b.fc2.bias.value[i] += w * std;
-        }
-    }
+    b = xor::add(&b, &mutation);
     b
-}
-
-fn crossover(a: &xor::XorModel, b: &xor::XorModel, mutation_rate: f32, std: f32) -> xor::XorModel {
-    let mut c = a.clone();
-
-    // fc1
-    // weights
-    for i in 0..a.fc1.weights.value.len() {
-        let r = thread_rng().gen::<f32>() % 1.0;
-        let m = thread_rng().gen::<f32>() % 1.0;
-        if r < 0.5 {
-            c.fc1.weights.value[i] = a.fc1.weights.value[i];
-        } else {
-            c.fc1.weights.value[i] = b.fc1.weights.value[i];
-        }
-
-        if m <= mutation_rate {
-            let w: f32 = thread_rng().sample(StandardNormal);
-            c.fc1.weights.value[i] += w * std;
-        }
-    }
-
-    // bias
-    for i in 0..a.fc1.bias.value.len() {
-        let r = thread_rng().gen::<f32>() % 1.0;
-        let m = thread_rng().gen::<f32>() % 1.0;
-        if r < 0.5 {
-            c.fc1.bias.value[i] = a.fc1.bias.value[i];
-        } else {
-            c.fc1.bias.value[i] = b.fc1.bias.value[i];
-        }
-
-        if m <= mutation_rate {
-            let w: f32 = thread_rng().sample(StandardNormal);
-            c.fc1.bias.value[i] += w * std;
-        }
-    }
-
-    // fc2
-    //weights
-    for i in 0..a.fc2.weights.value.len() {
-        let r = thread_rng().gen::<f32>() % 1.0;
-        let m = thread_rng().gen::<f32>() % 1.0;
-        if r < 0.5 {
-            c.fc2.weights.value[i] = a.fc2.weights.value[i];
-        } else {
-            c.fc2.weights.value[i] = b.fc2.weights.value[i];
-        }
-
-        if m <= mutation_rate {
-            let w: f32 = thread_rng().sample(StandardNormal);
-            c.fc2.weights.value[i] += w * std;
-        }
-    }
-
-    // bias
-    for i in 0..a.fc2.bias.value.len() {
-        let r = thread_rng().gen::<f32>() % 1.0;
-        let m = thread_rng().gen::<f32>() % 1.0;
-        if r < 0.5 {
-            c.fc2.bias.value[i] = a.fc2.bias.value[i];
-        } else {
-            c.fc2.bias.value[i] = b.fc2.bias.value[i];
-        }
-
-        if m <= mutation_rate {
-            let w: f32 = thread_rng().sample(StandardNormal);
-            c.fc2.bias.value[i] += w * std;
-        }
-    }
-    c
 }
 
 fn fitness(model: &xor::XorModel) -> (f32, bool) {
@@ -217,7 +108,7 @@ pub fn print_outputs(model: &xor::XorModel) {
 }
 
 pub fn train() {
-    let population = init_population(10000);
+    let population = init_population(1000);
     let mut info = score_population(&population);
 
     let mut g = 0;
@@ -230,28 +121,6 @@ pub fn train() {
             if i == 0 {
                 info.2[i].individual = old_population[info.3].individual.clone();
             } else {
-                /*
-                let mut parent_one_index = thread_rng().gen::<usize>() % info.2.len();
-                let mut parent_two_index = thread_rng().gen::<usize>() % info.2.len();
-
-                // parent 1
-                for _ in 0..5 {
-                    let r = thread_rng().gen::<usize>() % info.2.len();
-                    if old_population[r].score.0 > old_population[parent_one_index].score.0 {
-                        parent_one_index = r;
-                    }
-                }
-
-                // parent 2
-                for _ in 0..5 {
-                    let r = thread_rng().gen::<usize>() % info.2.len();
-                    if old_population[r].score.0 > old_population[parent_two_index].score.0 {
-                        parent_two_index = r;
-                    }
-                }
-
-                info.2[i].individual = crossover(&old_population[parent_one_index].individual, &old_population[parent_two_index].individual, 1.0, 0.05);
-                */
                 let mut parent_one_index = thread_rng().gen::<usize>() % info.2.len();
 
                 for _ in 0..5 {
@@ -261,7 +130,7 @@ pub fn train() {
                     }
                 }
 
-                info.2[i].individual = mutate(&old_population[parent_one_index].individual, 1.0, 0.05);
+                info.2[i].individual = mutate(&old_population[parent_one_index].individual, 0.05);
             }
         }
 
